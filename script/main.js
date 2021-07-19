70bb49b7479d546d500c76a6be5767b3183e807d
Rainy <72177240+rainingut@users.noreply.github.com>
2021-07-19 14:11:44 +0800

    測試看看能不能看到開發者工具的Vue tag

--- script/main.js
+++ script/main.js
@@ -19,3 +19,10 @@ const app = Vue.createApp({
   }
 })
  app.mount('#app');
+
+// 若是没有开启Devtools工具，在开发环境中开启，在生产环境中关闭
+if (process.env.NODE_ENV == 'development') {
+    Vue.config.devtools = true;
+} else {
+    Vue.config.devtools = false;
+}
